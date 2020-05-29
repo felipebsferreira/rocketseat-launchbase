83db46a4f7a7309d593292e980d54c1dbd938f8a
Felipe B. S. Ferreira <felipebsferreira@gmail.com>
2020-05-29 10:44:18 -0300

    Ajuste

--- Works/modulo_3/server.js
+++ Works/modulo_3/server.js
@@ -18,7 +18,7 @@ server.get("/", (request, response) => {
         avatar_url: "https://avatars2.githubusercontent.com/u/30900823?s=460&u=30f4e9405542d446de2b5772405d25158b6370c9&v=4",
         name: "Felipe Ferreira",
         role: "2º dan - Aikikai",
-        description: 'Professor da escola de Aikido Kyoukai Recife. Felipe Ferreira sensei é graduado 2º dan pela Aikikai e é aluno do Leonardo Sodré sensei, 5º Aikikai. Filiação: <a href="https://aikidopesquisa.wordpress.com" target="_blank">APA</a>',
+        description: 'Professor da escola de Aikido Kyoukai Recife. Felipe Ferreira sensei é graduado 2º dan pela Aikikai e é aluno do Leonardo Sodré sensei, 5º dan Aikikai. Filiação: <a href="https://aikidopesquisa.wordpress.com" target="_blank">APA</a>',
         links: [
             { name: "Github", url: "https://github.com/felipebsferreira" },
             { name: "Twitter", url: "https://twitter.com/felipbsferreira"},
